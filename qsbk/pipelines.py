7f895918c39da3ef6cea22f2cf415c3f564c8c3c
hua <1901210745@pku.edu.cn>
2020-02-06 14:46:47 +0800

    使用JsonItemExporter

--- qsbk/pipelines.py
+++ qsbk/pipelines.py
@@ -5,21 +5,24 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 import json
+from scrapy.exporters import JsonItemExporter
 
 
 class QsbkPipeline(object):
     def __init__(self):
-        self.fp = open("jianjie.json", "w", encoding='utf-8')
+        self.fp = open("jianjie.json", "wb")
+        self.exporter = JsonItemExporter(self.fp, ensure_ascii=False, encoding='utf-8')
+        self.exporter.start_exporting()
 
     @staticmethod
     def open_spider(self):
         print("爬虫开始啦。。。。。")
 
     def process_item(self, item, spider):
-        item_json = json.dumps(dict(item), ensure_ascii=False)
-        self.fp.write(item_json+'\n')
+        self.exporter.export_item(item)
         return item
 
     def close_spider(self, spider):
+        self.exporter.finish_exporting()
         self.fp.close()
         print("爬虫结束了。。。。。")
